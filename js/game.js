3e8553b3c139c25ab614cc9bb6c69b23c5afc518
eslamelkholy <eslamelkholy5325762@gmail.com>
2020-02-09 16:44:01 +0200

    test

--- js/game.js
+++ js/game.js
@@ -2,58 +2,16 @@ let images = ["./images/20.gif", "./images/30.gif", "./images/40.gif"];
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 const urlParams = window.location.search;
-<<<<<<< HEAD
-let levelval = localStorage.getItem("level");
-let speed = 0;
-let birdsArray = [];
-/* <div id="bomb">
-    <img src="/images/ezgif.com-crop.gif" alt="">
-</div> */
-
-class Bird {
-    constructor(top, src) {
-        let birdImg = document.createElement("img");
-        birdImg.setAttribute('draggable', false);
-        this.bird = birdImg;
-        this.bird.src = src;
-        this.bird.classList.add("bird");
-        // this.bird.classList.add("flip");
-=======
-let levelval = getSecondPart(urlParams);
-let speed = 0;
-let birdsArray = [];
-class Bird {
-    constructor(top, src) {
-        let birdImg = document.createElement("img");
-        this.bird = birdImg;
-        this.bird.src = src;
-        this.bird.classList.add("bird");
->>>>>>> eec9f2bf1ad4f07a8a480843a70755e97234a6a8
-        this.bird.style.top = top + "px";
-        this.bird.style.left = "100%";
-        this.myInterval;
-    }
-    addtoParent = function () {
-        var body = document.querySelector("body");
-        body.appendChild(this.bird);
-    }
     moveLeft = function () {
 
         let counter = 100;
         this.bird.style.right = parseInt(this.bird.style.right) + counter + "px";
         //  this.bird.style.right = 10+"px";
         $(this.bird).animate({
-<<<<<<< HEAD
-            // right: "+=200"
-            left: "-10%"
-        }, 5000)
-=======
             left: "-10%"
         }, speed, function () {
             $(this).remove()
-
         })
->>>>>>> eec9f2bf1ad4f07a8a480843a70755e97234a6a8
 
 
 
@@ -62,56 +20,10 @@ class Bird {
         return this.bird.style.right;
 
     }
-<<<<<<< HEAD
-    removeBird = function () {
-        // $(".bird").hide();
-        // var body = document.querySelector("body");
-        // var myImg=document.querySelector(".bird")
-        // console.log(myImg)
-        // body.removeChild(myImg);
-        // this.bird.style.display = "none";
-
-
-    }
-=======
->>>>>>> eec9f2bf1ad4f07a8a480843a70755e97234a6a8
 }
 
 switch (levelval) {
     case "level1":
-<<<<<<< HEAD
-        speed = 2000;
-        break;
-    case "level2":
-        speed = 1000;
-        break;
-
-}
-
-//moving birds
-// window.setInterval(function () {
-//     for (let i = 0; i < birdsArray.length; i++) {
-//         birdsArray[i].moveLeft();
-//         // let counter = 100;
-//         // birdsArray[i].style.right = parseInt(this.bird.style.right) + counter + "px";
-//     }
-
-// }, 1000)
-//code for removing newly created objects
-window.setInterval(function () {
-    for (let i = 0; i < birdsArray.length; i++) {
-        if (parseInt(birdsArray[i].bird.style.right) == (windowWidth)) {
-            // $(birdsArray[i].bird).remove();
-            // console.log($(birdsArray[i]));
-            birdsArray.splice(birdsArray[i]);
-        }
-    }
-
-    // console.log(birdsArray.length);
-    // birdsArray.shift();
-    // console.log(birdsArray.length);
-}, 1500);
-=======
         speed = 7000;
         break;
     case "level2":
@@ -126,7 +38,6 @@ window.setInterval(function () {
 //         birdsArray.shift();
 //     }
 // }, 50);
->>>>>>> eec9f2bf1ad4f07a8a480843a70755e97234a6a8
 
 function getSecondPart(str) {
     return str.split('levels=')[1];
