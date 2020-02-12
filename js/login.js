78169ab7de598a97092ccf81e396185d4b31ba4d
Esraa <esraa.abdelaziz141@gmail.com>
2020-02-12 16:35:35 +0200

    leader board updates

--- js/login.js
+++ js/login.js
@@ -1,17 +1,19 @@
 let arr = [];
 let tbodyObj = document.querySelector("tbody");
-for (var i = 0; i < localStorage.length; i++) {
-    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-}
-arr.sort((a, b) => a.score - b.score);
-for (var i = 0; i <arr.length; i++) {
-    let trObject = document.createElement("tr");
-    let tdObjectName = document.createElement("td");
-    let tdObjectScore=document.createElement("td");
-    tdObjectName.innerText = arr[i].name;
-    tdObjectScore.innerText=arr[i].score;
-    trObject.appendChild(tdObjectName);
-    trObject.appendChild(tdObjectScore);
-    tbodyObj.appendChild(trObject);
+if (localStorage.length != 0) {
+    for (var i = 0; i < localStorage.length; i++) {
+        arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+    }
+    arr.sort((a, b) => a.score - b.score);
+    for (var i = 0; i < arr.length; i++) {
+        let trObject = document.createElement("tr");
+        let tdObjectName = document.createElement("td");
+        let tdObjectScore = document.createElement("td");
+        tdObjectName.innerText = arr[i].name;
+        tdObjectScore.innerText = arr[i].score;
+        trObject.appendChild(tdObjectName);
+        trObject.appendChild(tdObjectScore);
+        tbodyObj.appendChild(trObject);
 
+    }
 }
