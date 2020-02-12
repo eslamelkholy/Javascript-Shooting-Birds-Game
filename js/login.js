874bf23f2ce362c624291bee101075ac355230a4
Esraa <esraa.abdelaziz141@gmail.com>
2020-02-12 16:22:49 +0200

    leader board

--- js/login.js
+++ js/login.js
@@ -1,7 +1,17 @@
-let arr=[];
-for(obj in window.localStorage){
-console.log(window.localStorage[obj])
-if( window.localStorage[obj] instanceof object){
-    arr.push(window.localStorage[obj]);
+let arr = [];
+let tbodyObj = document.querySelector("tbody");
+for (var i = 0; i < localStorage.length; i++) {
+    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
 }
+arr.sort((a, b) => a.score - b.score);
+for (var i = 0; i <arr.length; i++) {
+    let trObject = document.createElement("tr");
+    let tdObjectName = document.createElement("td");
+    let tdObjectScore=document.createElement("td");
+    tdObjectName.innerText = arr[i].name;
+    tdObjectScore.innerText=arr[i].score;
+    trObject.appendChild(tdObjectName);
+    trObject.appendChild(tdObjectScore);
+    tbodyObj.appendChild(trObject);
+
 }
